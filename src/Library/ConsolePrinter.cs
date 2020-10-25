cb3e626e0ba9bea280c3edf94e1257ad80426656
ilima12091 <limaignacio12091@gmail.com>
2020-10-24 22:47:04 -0300

    Agrego los archivos

---
+++ src/Library/ConsolePrinter.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    /*
+        Esta clase implementa el tipo IPrinter y cumple con el principio LSP ya que cualquier objeto de ella va poder ser asignado
+        a una variable del tipo IPrinter sin afectar el comportamiento del programa.
+    */
+    public class ConsolePrinter : IPrinter
+    {
+        public void PrintRecipe(Recipe recipe)
+        {
+            Console.WriteLine(recipe.GetTextToPrint());
+        }
+    }
+}
